5ec88d9d470cefa083becce0633ad27a74373386
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2023-12-08 16:34:57 +0800

    Added Users Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ArticlesModule } from './articles/articles.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [DatabaseModule, ArticlesModule],
+  imports: [DatabaseModule, ArticlesModule, UsersModule],
   controllers: [],
   providers: [],
 })
