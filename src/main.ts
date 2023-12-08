51eca168a8b50d84087c8caf5083ab08a8b1514e
Joshua Concepcion <72645494+Molted@users.noreply.github.com>
2023-12-08 16:34:25 +0800

    Added ClassSerializeInterceptor to transform response data

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { PrismaClientExceptionFilter } from './common/filters/prisma-client-exception/prisma-client-exception.filter';
 
 async function bootstrap() {
@@ -15,8 +15,9 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
-  const { httpAdapter  } = app.get(HttpAdapterHost);
+  const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 
   const document = SwaggerModule.createDocument(app, config);
